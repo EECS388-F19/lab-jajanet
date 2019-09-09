65612b7701f6813b6ed6a90fd597678dbc4ef2e6
jajanet <vjanet@umich.edu>
2019-09-09 14:36:44 -0400

    add working script to lab0

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+from random import randint
+print("Janet")
+a = randint(0,100) 
+b = randint(0,100)
+both = a + b
+print(str(a) + " " + str(b))
+print("Sum = " + str(both))
+print("Average = " + str(both/2))
